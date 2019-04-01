fe63a6e47c8dfff72861053a5ad6a9e63e499077
Álvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-04-01 20:18:29 +0100

    adding ViewExtension template

---
+++ DynamoDev.ViewExtension/src/$projectname$Window.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Interaction logic for $projectname$Window.xaml
+    /// </summary>
+    public partial class $projectname$Window : Window
+    {
+        public $projectname$Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
